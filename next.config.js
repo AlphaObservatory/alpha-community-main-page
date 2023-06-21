619fbe6881087e109891721bbdb55d54c65d5355
Matteo Papadopoulos <m.papadopoulos@cantierecreativo.net>
2023-06-21 13:12:24 +0200

    Add locales to config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 
 module.exports = {
+  i18n: {
+    locales: ["en", "it"],
+    defaultLocale: "en",
+  },
   env: {
     NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN:
       process.env.NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN,
